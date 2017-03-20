3bcf778acfce58abf736da804d7e5ad5ae1a04d6
Samuel D. Bristol <samuel.d.bristol@gmail.com>
2017-03-20 03:05:10 -0500

    update layout for index and about pages

--- js/init.js
+++ js/init.js
@@ -1,9 +1,30 @@
+/* jslint esversion: 6 */
 (function($){
   $(function(){
 
+    // Navigation initialization
     $('.button-collapse').sideNav();
 
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    // Mobile navbar close botton handler
+    $('#closeMobileNav').click( () => {
+      $('.button-collapse').sideNav('hide');
+    });
+
+    // Index slider init
     $('.slider').slider();
+
+    // Init any scrollspy elements
+    $('.scrollspy').scrollSpy();
+
+    // Init any pushpin elements
+    $('#pushpin-navbar').pushpin({
+      top: $('#pushpin-navbar').offset().top
+    });
+    $('#pushpin-toc').pushpin({
+      top: $('#pushpin-toc').offset().top,
+      offset: 75
+    });
+
+    Materialize.showStaggeredList("#content-items");
   }); // end of document ready
 })(jQuery); // end of jQuery name space
